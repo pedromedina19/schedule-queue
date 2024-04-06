7e24afe8c64e45479c75adb949d168c5819272f6
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-04-06 19:32:43 -0300

    implementing a "cronjob"

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Tweet } from '../entities/tweet.entity';
+import { Interval } from '@nestjs/schedule';
 
 @Injectable()
-export class TweetsCountService {}
+export class TweetsCountService {
+    constructor(
+        @InjectModel(Tweet)
+        private tweetModel: typeof Tweet){}
+    
+    @Interval(5000)
+    async countTweets() {
+        console.log('executou');
+    }
+
+}
